0a4acfb6e5ab04d9d4bce4208ecf001901a206b3
Aimene bahri <bahri.aimen48@gmail.com>
2020-10-03 10:48:36 +0100

    add some extra change on the style

---
+++ script.js
@@ -0,0 +1,7 @@
+var start = 1990;
+var end = new Date().getFullYear();
+var options = "";
+for(var year = start ; year <=end; year++){
+  options += "<option>"+ year +"</option>";
+}
+document.getElementById("year").innerHTML = options;
